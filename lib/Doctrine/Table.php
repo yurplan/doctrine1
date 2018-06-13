cf27b7aa43e4bee44073e84af332185cf7cc97c9
Dennis Verspuij <dennisverspuij@users.noreply.github.com>
2018-06-13 09:34:36 +0000

    Skip options charset and collation for field validation

--- lib/Doctrine/Table.php
+++ lib/Doctrine/Table.php
@@ -2655,6 +2655,8 @@ class Doctrine_Table extends Doctrine_Configurable implements Countable, Seriali
                     || $name == 'fixed'
                     || $name == 'comment'
                     || $name == 'alias'
+                    || $name == 'charset'
+                    || $name == 'collation'
                     || $name == 'extra') {
                 continue;
             }
