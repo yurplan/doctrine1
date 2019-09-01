6b38c1fd2c19354b6a51f8dcad73ed4a9c603ea0
Alexandre Quercia <alquerci@email.com>
2019-09-02 00:27:03 +0200

    Fix PHP 7.4 compatibility

--- tests/BaseTestCase.php
+++ tests/BaseTestCase.php
@@ -92,24 +92,24 @@ class Doctrine_Base_TestCase extends Doctrine_UnitTestCase
         $this->assertTrue(file_exists($modelFiles['ConservativeModelLoadingContact']));
     }
 
-    public function testGetConnectionByTableName()
+    public function testGetConnectionByTableNameForTableWithOneModel()
     {
         $conn = null;
         $thrownException = null;
 
         try {
-            $connectionBefore = Doctrine_Core::getConnectionByTableName('entity');
+            $connectionBefore = Doctrine_Core::getConnectionByTableName('account');
 
             $conn = Doctrine_Manager::connection('sqlite::memory:', 'test_memory');
-            Doctrine_Manager::getInstance()->bindComponent('Entity', 'test_memory');
+            Doctrine_Manager::getInstance()->bindComponent('Account', 'test_memory');
 
-            $connectionAfter = Doctrine_Core::getConnectionByTableName('entity');
+            $connectionAfter = Doctrine_Core::getConnectionByTableName('account');
 
             $this->assertEqual($connectionAfter->getName(), 'test_memory');
 
-            Doctrine_Manager::getInstance()->bindComponent('Entity', $connectionBefore->getName());
+            Doctrine_Manager::getInstance()->bindComponent('Account', $connectionBefore->getName());
 
-            $connectionAfter = Doctrine_Core::getConnectionByTableName('entity');
+            $connectionAfter = Doctrine_Core::getConnectionByTableName('account');
 
             $this->assertEqual($connectionBefore->getName(), $connectionAfter->getName());
         } catch (Throwable $e) {
